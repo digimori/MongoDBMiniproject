c4220e5510e04eab2086d96a2018b2191ca72156
digimori <hanaorikuu@gmail.com>
2023-03-06 17:11:03 +0000

    Logout functionality added

--- app.py
+++ app.py
@@ -87,6 +87,14 @@ def profile(username):
     return render_template("profile.html", username=username)
 
 
+@app.route("/logout")
+def logout():
+    # Remove session cookie and redirect to login page
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
