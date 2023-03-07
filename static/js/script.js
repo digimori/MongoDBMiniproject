154ec20a465cf6812c7dcec5ef47daed988ab527
digimori <hanaorikuu@gmail.com>
2023-03-07 16:06:14 +0000

    Is urgent toggle and datepicker added to add task page. Initialized in jQuery

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.collapsible').collapsible();
   });
-       
+
+  
+  $('#textarea1').val('New Text');
+  M.textareaAutoResize($('#textarea1'));
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 3,
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+    });
+  });
+        
